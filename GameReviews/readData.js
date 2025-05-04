54847a4f2ed719097b98abf8844556610ff543df
DarthC0mp1ler <55661326+DarthC0mp1ler@users.noreply.github.com>
2025-05-04 20:48:34 +0200

    review reading done

---
+++ GameReviews/readData.js
@@ -0,0 +1,83 @@
+
+
+function getLinks(path) {
+    console.log(path)
+    fetch(path)
+        .then(res => res.text())
+        .then(html => {
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(html, 'text/html');
+            const links = Array.from(doc.querySelectorAll('a'))
+                .map(link => link.getAttribute('href'))
+                .filter(href => href.endsWith('.xml'));
+            console.log(links);
+
+            var element = document.getElementById("titles");
+            links.forEach(link => {
+                const a = document.createElement('a');
+                a.href = `../../reviewTemplate.html?file=${encodeURIComponent(link)}`;
+                a.textContent = link
+                    .split('review_')[1]
+                    .split('.xml')[0]
+                    .replaceAll('_', ' ')
+                    .replaceAll(/\s+/g, ' ');
+                a.classList.add('home-link');
+                element.appendChild(a);
+            })
+
+
+        });
+}
+
+
+function loadReviewPage() {
+    const params = new URLSearchParams(location.search);
+    const file = params.get('file');
+    console.log(file)
+    fetch(`../${encodeURIComponent(file)}`)
+        .then(res => res.ok ? res.text() : Promise.reject('File not found'))
+        .then(xmlText => {
+            const parser = new DOMParser();
+            const xmlDoc = parser.parseFromString(xmlText, "text/xml");
+
+            var image = xmlDoc.querySelector('image')?.textContent;
+            var title = xmlDoc.querySelector('title')?.textContent;
+            var rating = xmlDoc.querySelector('rating')?.textContent;
+            var genre = xmlDoc.querySelector('genre')?.textContent;
+            var releaseData = xmlDoc.querySelector('releaseDate')?.textContent;
+            var summary = xmlDoc.querySelector('summary')?.innerHTML;
+            var pros = xmlDoc.querySelector('pros').innerHTML;
+            var cons = xmlDoc.querySelector('cons').innerHTML;
+            var po = xmlDoc.querySelector('po')?.textContent.trim();
+            po = po.split('\n')
+                .filter(paragraph => paragraph !== '')
+                .map(paragraph => '<p>' + paragraph.trim() + '</p>')
+                .join("\n");
+
+            var container = document.getElementById('bg-image')
+            container.style.backgroundImage = container.style.backgroundImage.replace('@image', image);
+
+            container = document.getElementById('title');
+            container.innerHTML = container.innerHTML.replace('@title', title)
+
+            container = document.getElementById('rating');
+            container.innerHTML = container.innerHTML.replace('@rating', rating)
+
+            container = document.getElementById('genre');
+            container.innerHTML = container.innerHTML.replace('@genre', genre)
+
+            container = document.getElementById('releaseDate');
+            container.innerHTML = container.innerHTML.replace('@releaseDate', releaseData)
+
+            container = document.getElementById('summary');
+            container.innerHTML = container.innerHTML.replace('@summary', summary)
+
+            container = document.getElementById('pros-cons');
+            container.innerHTML = container.innerHTML.replace('@pros', pros)
+            container.innerHTML = container.innerHTML.replace('@cons', cons)
+
+            container = document.getElementById('opinion');
+            container.innerHTML = container.innerHTML.replace('@po', po)
+        })
+}
+
